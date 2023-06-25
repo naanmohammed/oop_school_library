a445db51afb940060f5ed62e3fc1f54c5e3d08e1
naanmohammed <naanahmed24@gmail.com>
2023-06-25 21:12:02 +0300

    Add rental class

---
+++ rental.rb
@@ -0,0 +1,15 @@
+require_relative 'person'
+require_relative 'book'
+
+class Rental
+  attr_accessor :date, :person, :book
+
+  def initialize(date, person, book)
+    @date = date
+    @person = person
+    @book = book
+
+    person.rentals << self
+    book.rentals << self
+  end
+end
