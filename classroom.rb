96dd34d6b4089fd7beae33ad115f693dd12a5919
naanmohammed <naanahmed24@gmail.com>
2023-06-25 22:58:12 +0300

    update classroom.rb

--- classroom.rb
+++ classroom.rb
@@ -2,15 +2,16 @@
 require_relative 'student'
 
 class Classroom
-  attr_accessor :label, :students
+  attr_accessor :label
+  attr_reader :students
 
   def initialize(label)
     @label = label
     @students = []
   end
 
-  def add_student(age, name = 'Unknown', parent_permission: true)
-    student = Student.new(age, self, parent_permission: parent_permission, name: name)
-    @students << student
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
   end
 end
