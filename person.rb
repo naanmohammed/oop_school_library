6245256ae375f84cfe0dbfbc5393f02efd81f297
naanmohammed <naanahmed24@gmail.com>
2023-06-26 22:43:37 +0300

    Update person class

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
-  attr_accessor :name, :age 
-  attr_reader :id 
+  attr_accessor :name, :age
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = generate_id
@@ -8,11 +8,7 @@ class Person
     self.age = age
     @parent_permission = parent_permission
   end
-
-  def age=(age)
-    @age = age.to_i
-  end
-
+  
   def can_use_services?
     of_age? || @parent_permission
   end
