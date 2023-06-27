f7b1b8dcdd4e1e82b9251861b25b67c9bfdb3e65
naanmohammed <naanahmed24@gmail.com>
2023-06-28 00:49:42 +0300

    Update student class to fix errors

--- student.rb
+++ student.rb
@@ -1,21 +1,19 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :name
   attr_reader :classroom
 
-  def initialize(age, classroom, parent_permission: true, name: 'Unknown')
-    super(name, age, parent_permission)
-
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
-  end
-
-  def play_hooky
-    '¯\(ツ)/¯'
   end
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+    classroom.students.push(self)
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
   end
 end
